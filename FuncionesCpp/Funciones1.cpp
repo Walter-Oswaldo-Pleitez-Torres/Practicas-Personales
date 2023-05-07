dd7a9ce25cb58211e60f7be1368d0ca5a08c193e
Walter-Oswaldo-Pleitez-Torres <00143023@uca.edu.sv>
2023-05-07 14:58:53 -0600

    V4 - add - Funciones

---
+++ FuncionesCpp/Funciones1.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+//PROTOTIPO DE FUNCION
+int encontrarMax(int x, int y);
+
+int main(){
+    int n1, n2, mayor;
+    cout<<"Digite 1 numero: "<<endl;
+    cin>>n1;
+    cout<<"Digite uno mas: "<<endl;
+    cin>>n2;
+
+    mayor = encontrarMax(n1, n2);
+    cout<<"El numero mayor es: "<<mayor<<endl;    
+
+    getch();
+    return 0;
+}
+
+//DEFINICION DE LA FUNCION
+int encontrarMax(int x, int y){
+    int numMax;
+
+    if(x>y){
+        numMax = x;
+    }
+    else{
+        numMax = y;
+    }
+
+    return numMax;
+}
